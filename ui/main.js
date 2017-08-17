fcd50fda1d46df3cf07b4ef5d67a9f5e2f8d8d0f
SangamnathH <30953098+SangamnathH@users.noreply.github.com>
2017-08-17 22:12:00 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
 
-var element = document.getElementbyId("main-text");
+var element = document.getElementById ('main-text');
+element.innerHTML = 'new Value';
 
-element.innerhtml = 'new value';
