ab3bec81cd3388cc890a7ae2a6b53a7987ea6d64
SangamnathH <30953098+SangamnathH@users.noreply.github.com>
2017-08-17 22:40:41 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ element.innerHTML = 'new Value';
 var img = document.getElementById ('madi');
 var marginLeft = 0;
 function moveRight () {
-    marginLeft = marginLeft + 5;
+    marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
 
