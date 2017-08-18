58ccccb9827ea2acf66eaafa1a960cd8d2d1bb41
SangamnathH <30953098+SangamnathH@users.noreply.github.com>
2017-08-18 22:13:48 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,13 @@
-console.log('Loaded!');
+//counter code
+var button = document.getElementById("counter");
+var counter =0;
 
-var element = document.getElementById ('main-text');
-element.innerHTML = 'new Value';
-
-//move image
-var img = document.getElementById ('madi');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
+button.onclick = function() {
+    
+    counter = counter+0;
+    var span = document.getElementById("count");
+    span.innerHTML = counter.toString();
+    
 }
 
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
-    };
+
