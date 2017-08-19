dfa98abc1582dda78ccd70c60ace4ad51b8031db
SangamnathH <30953098+SangamnathH@users.noreply.github.com>
2017-08-19 18:37:48 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,8 +9,9 @@ button.onclick = function() {
         if (request.readyState == XMLHttpRequest.DONE)
         {
             if(request.status == 200) {
-                 //Capture a list of names and rendet as a list
-    
+                 //Capture a response and render as a list.
+                var counter = request.responseText;
+                var span = 
                 var names = request.responseText;
                 names = JSON.parse(names);
                 var list ='';
@@ -22,27 +23,14 @@ button.onclick = function() {
             }
         }
     };
-    
+    var name = nameInput.value;
+    var submit = document.getElementById("submit_btn");
     request.open('GET', 'http://sangamnath.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 };
 
 var nameInput = document.getElementById("name");
 var name = nameInput.value;
-var submit = document.getElementById("submit_btn");
-submit.onclick = function() {
-    //Make a request to the server and send the name
-    //Capture a list of names and rendet as a list
-    
-    var names = ['name1', 'name2', 'name3', 'name4'];
-    var list ='';
-    for (var i=0; i< names.length; i++) {
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById("namelist");
-    ul.innerHTML = list;
-    
-    
 };
 
 
