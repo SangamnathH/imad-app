56d3272a2b2ed7abfa2b73308c396237c051d8fb
SangamnathH <30953098+SangamnathH@users.noreply.github.com>
2017-08-19 18:26:25 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,7 +23,7 @@ button.onclick = function() {
         }
     };
     
-    request.open("GET", "http://sangamnath.imad.hasura-app.io/submit-name?name=" + name, true);
+    request.open('GET', 'http://sangamnath.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 };
 
