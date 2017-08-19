7fafe86f29fc1b9b13ece80cb01ceeae16edaa3e
SangamnathH <30953098+SangamnathH@users.noreply.github.com>
2017-08-19 19:30:33 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,13 +30,13 @@ submit.onclick = function () {
     
     //Capture the response and store it in a variable
     request.onreadystatechange = function() {
-        if (request.readyState == XMLHttpRequest.DONE) {
+        if (request.readyState === XMLHttpRequest.DONE) {
             
             if(request.status === 200) {
-              var names =request.responseText;
+              var names = request.responseText;
               names = JSON.parse(names);
               var list = '';
-                for (var i=0; i< names.length; i++) {
+              for (var i=0; i< names.length; i++) {
                     list += '<li>' + names[i] + '</li>';
             }
             var ul = document.getElementById('nameslist');
