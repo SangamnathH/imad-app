acda453d7ccf6d84eac84fd039fdeae25fbd8f04
SangamnathH <30953098+SangamnathH@users.noreply.github.com>
2017-08-19 19:23:13 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -47,9 +47,8 @@ submit.onclick = function () {
         }
         // Not done yet
     };
-    var nameInput = document.getElementById("name");
+    var nameInput = document.getElementById('name');
     var name = nameInput.value;
-
     //Make a request
     request.open('GET', 'http://sangamnath.imad.hasura-app.io/submit-name?name='+ name, true);
     request.send(null);
