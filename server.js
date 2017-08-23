3d6f20d4a8e18edc3d923d0c1e1b897a905b1ea0
SangamnathH <30953098+SangamnathH@users.noreply.github.com>
2017-08-23 22:26:09 +1000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,11 +99,11 @@ app.post('/login', function(req, res) {
         if (err) {
             res.status(500).send(err.toString());
         } else {
-            if (results.rows.length === 0) {
+            if (result.rows.length === 0) {
                 res.send(403).send("username/password is invalid");
             } else {
                 // Match the password
-                var dbString = results.rows[0].password;
+                var dbString = result.rows[0].password;
                 var salt = dbString.split('$')[2];
                 var hashedPassword = hash(password, salt); //Creating a hash based on the password submitted and the original salt
                 if (hashedPassword === dbString) {
