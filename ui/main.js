048bef98d6d302d353e4b92f5427feeaa7e2f061
SangamnathH <30953098+SangamnathH@users.noreply.github.com>
2017-08-25 19:54:08 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,24 +10,20 @@ submit.onclick = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             
             if(request.status === 200) {
-              var names = request.responseText;
-              names = JSON.parse(names);
-              var list = '';
-              for (var i=0; i< names.length; i++) {
-                    list += '<li>' + names[i] + '</li>';
+                console.login('user logged in');
+                alert('logged in successfully');
             }
             var ul = document.getElementById('namelist');
             ul.innerHTML = list;
-      
-               
-            }
         }
         // Not done yet
     };
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
     //Make a request
-    request.open('POST', 'http://sangamnath.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.open('POST', 'http://sangamnath.imad.hasura-app.io/login' + name, true);
     request.send(JSON.stringify({username: username, password: password}));
     
 };
