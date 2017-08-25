2534eb9d7f7ae8cf8a8dcc472655f0a924df17ea
SangamnathH <30953098+SangamnathH@users.noreply.github.com>
2017-08-25 19:45:27 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,27 +1,4 @@
-//counter code
-var button = document.getElementById("counter");
-
-button.onclick = function() {
-    
-    var request = new XMLHttpRequest();
-    
-    //Capture the response and store it in a variable
-    request.onreadystatechange = function() {
-        if (request.readyState == XMLHttpRequest.DONE) {
-            
-            if(request.status === 200) {
-                
-                var counter = request.responseText;
-                var span = document.getElementById("count");
-                span.innerHTML = counter.toString();
-            }
-        }
-    };
-    request.open('GET', 'http://sangamnath.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
-
-//Submit name
+//Submit user name
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     
